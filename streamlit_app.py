c5819d4f3eff9799bbc93b63ab5bfa17a557259a
yendada <truecallervizag@gmail.com>
2023-04-28 16:05:32 +0100

    Update streamlit_app.py
    -Added Streamlit.text lines

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,10 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
+
+
+streamlit.text('Omega 3 & Blueberry Oat meal')
+
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+
+streamlit.text('Hard-Boiled Free-Range Egg')
