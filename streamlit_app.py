4820e16424cf78128273657ff222c0dd501ecae5
yendada <truecallervizag@gmail.com>
2023-04-30 09:57:03 +0100

    Update streamlit_app.py
    -  we'll pre-populate the list to set an example for the customer for Picking fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,8 +24,9 @@ import pandas as pd
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
+
 # Let's put a pick list here so they can pick the fruit they want to include 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), [ 'Pineapple', 'Cherries'] )
 
 
 # Display the table on the page.
