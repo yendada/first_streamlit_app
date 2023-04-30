9c10dd7c9f2b1e975dd2c7d079b122bebdce6915
yendada <truecallervizag@gmail.com>
2023-04-30 09:48:04 +0100

    Update streamlit_app.py
    - Imported Pandas
    - Using Padas in Streamlit to read a CSV in a data frame.

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,3 +17,10 @@ streamlit.text('🥑🍞 Avacado Toast')
 
 
 streamlit.header ('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+
+import pandas as pd
+
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+streamlit.dataframe(my_fruit_list)
