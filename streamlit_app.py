1b48a26da593f34d487e864eac4f7971d4331b48
yendada <truecallervizag@gmail.com>
2023-04-30 09:38:46 +0100

    Update streamlit_app.py
    Added menu images

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,11 +4,13 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header('Breakfast Menu')
+streamlit.header('Breakfast Favourites')
 
 
-streamlit.text('Omega 3 & Blueberry Oat meal')
+streamlit.text('🥣 Omega 3 & Blueberry Oat meal')
 
-streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+
+streamlit.text('🥑🍞 Avacado Toast')
