9788703caeef0fa2e54897fbe197cb650e668a6f
yendada <truecallervizag@gmail.com>
2023-05-14 09:07:08 +0100

    Update streamlit_app.py
    Connecting Snoflake with Streamlit and querying Snowflake to get account details

--- streamlit_app.py
+++ streamlit_app.py
@@ -64,6 +64,11 @@ streamlit.dataframe(fruityvice_normalized)
 #Importing required packages indicated in requirements.txt into snowflake via importing snowflake dot connector
 import snowflake.connector
 
-
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
 
 
