40bc6085f98e75e6c43f4b259c213232bd103baf
yendada <truecallervizag@gmail.com>
2023-05-16 01:55:46 +0100

    Update streamlit_app.py
    - Close the connection
    - Updated Header

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,7 +60,7 @@ streamlit.stop()
 #Importing required packages indicated in requirements.txt into snowflake via importing snowflake dot connector
 #import snowflake.connector
 
-streamlit.header("The fruit load list contains:")
+streamlit.header("View Our Fruit List - Add Your Favorites!")
 #Snowflake related functions
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
@@ -68,9 +68,10 @@ def get_fruit_load_list():
          return my_cur.fetchall()
 
 #Add a button to load the fruit
-if streamlit.button('Get Fruit load list'):
+if streamlit.button('Get Fruit list'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows = get_fruit_load_list()
+    my_cnx.close()
     streamlit.dataframe(my_data_rows)
 
 #Allow the end user to a fruit to the list
