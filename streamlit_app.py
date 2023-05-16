03284a3c3be2750fa3127cc260b0a3f33ab9019f
yendada <truecallervizag@gmail.com>
2023-05-16 01:04:13 +0100

    Update streamlit_app.py
    Moving the Fruityvice Code into a Try-Except (with a nested If-Else)

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,29 +41,19 @@ streamlit.dataframe(fruits_to_show)
 # Get the Fruityvice Data Looking a Little Nicer:
 
 # New section to display Fruityvice api response
-
 streamlit.header('Fruityvice Fruit Advice!')
+try:
+    fruit_choice = streamlit.text_input('What fruit would you like information about?')
+    if not fruit_choice:
+        streamlit.error("Please select a fruit to get information.")
+    else:
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+        fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+        streamlit.dataframe(fruityvice_normalized)
 
 
-#Add a Text Entry Box and Send the Input to Fruityvice as Part of the API Call
-
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-
-
-#import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
-# streamlit.text(fruityvice_response.json()) #Removing displayng raw json 
-
-
-  
-# Get the JSON version of the Fruityvice response and normalize it.
-fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-
-# Diplaying the normalized data in a data frame ie a table.
-streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+    streamlit.error()
 
 streamlit.stop()
 
