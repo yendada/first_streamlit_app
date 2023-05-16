c35fc04936250c8099af5569be787acfdbe661de
yendada <truecallervizag@gmail.com>
2023-05-16 01:17:50 +0100

    Update streamlit_app.py
    corrected indentation in return statement

--- streamlit_app.py
+++ streamlit_app.py
@@ -65,7 +65,7 @@ streamlit.header("The fruit load list contains:")
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
          my_cur.execute("SELECT * from fruit_load_list")
-        return my_cur.fetchall()
+         return my_cur.fetchall()
 
 #Add a button to load the fruit
 if streamlit.button('Get Fruit load list'):
